520f591ba024f15dd775f926ba50ff6ef90313fc
Markus Borer <markus.borer@adesso.ch>
2018-11-29 16:02:21 +0100

    Uebung 8

--- reactsample/src/PersonSearchPanel.js
+++ reactsample/src/PersonSearchPanel.js
@@ -3,8 +3,9 @@ import PersonSearchForm from './PersonSearchForm';
 import PersonSearchResult from './PersonSearchResult';
 import axios from 'axios';
 import ErrorBoundary from './ErrorBoundary';
-import { fromEvent } from 'rxjs';
+import { fromEvent, of } from 'rxjs';
 import { debounceTime, filter, switchMap } from 'rxjs/operators'
+import { Modal } from 'react-materialize';
 
 class PersonSearchPanel extends React.Component {
 
@@ -13,7 +14,8 @@ class PersonSearchPanel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            persons: []
+            persons: [],
+            error: undefined
         }
     }
 
@@ -24,12 +26,23 @@ class PersonSearchPanel extends React.Component {
                 //filter(event => event.detail.length > 3),
                 switchMap(event => {
                     console.log('Event: ' + event.detail);
-                    return axios.get('http://localhost:8080/person?name=' + event.detail);
+                    return axios.get('http://localhost:8080/person?name=' + event.detail).catch(error => {
+                        console.log(error);
+                        this.setState({
+                            error: 'Fehler bei der Serverabfrage'
+                        });
+                        return of(undefined);
+                    });
                 })
             )
             .subscribe(response => {
-                console.log('Response: ' + response);
-                this.setState({persons: response.data});
+                if (response.data !== undefined) {
+                    console.log('Response: ' + response);
+                    this.setState({
+                        persons: response.data,
+                        error: undefined
+                    });
+                }
             });
     }
 
@@ -39,21 +52,13 @@ class PersonSearchPanel extends React.Component {
         );
     }
 
-    /*
-    onChange = (text) => {
-        axios.get('http://localhost:8080/person?name=' + text)
-            .then(res => {
-                this.setState({persons: res.data});
-            })
-    }
-    */
-
     render() {
         return (
             <ErrorBoundary>
                 <h2 className="left-align">Personensuche</h2>
                 <PersonSearchForm onChange={this.onChange} />
-                <PersonSearchResult persons={this.state.persons} />
+                {this.state.error === undefined && <PersonSearchResult persons={this.state.persons} />}
+                {this.state.error !== undefined && <Modal header='Error' open='true'>{this.state.error}</Modal>}
             </ErrorBoundary>
         )
     }
